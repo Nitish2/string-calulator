66c03ab9d66a0526f02695ba518f692c8fc18b37
Nitish Negi <nitihsn886@gmail.com>
2024-09-08 17:46:09 +0530

    final code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  beforeEach(() => { TestBed.configureTestingModule({
+    imports: [RouterTestingModule,FormsModule],
+    declarations: [AppComponent],
+  });
+  fixture = TestBed.createComponent(AppComponent);
+  component = fixture.componentInstance;
+  fixture.detectChanges();
+});
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = component;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'string-calculator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = component;
     expect(app.title).toEqual('string-calculator');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('string-calculator app is running!');
+  it(`should have return' add function'`, () => {
+    component.output=0
+    const result = component.add('1/n,23')
+    expect(component.add('123')).toEqual(6);
   });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'string-calculator';
+  constructor(){}
+  numbers:string='';
+  output:number=0;
+  ngOnInit(): void {}
+
+  add(str:string){
+    let newStr = str.replace(/\D/gi, '')
+    const arr = [...newStr];
+    if (str.length > 0){
+     this.output = this.strToInt(arr);
+    } else {
+     this.output = 0;
+    }
+    return this.output;
+}
+strToInt(arr:any){
+  return arr.reduce((acc:any, cur:any) => acc + Number(cur), 0);
+}
+
+changeInput(event:any){
+ this.output =  event.length === 0 ? 0 :this.output;
+}
 }
